166baf20bfdb0a1026b0178ca2525551d7a3954a
Nick Oates <nick@nickoates.com>
2024-09-05 11:07:35 -0700

    Update AI SDK

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import { env } from 'hono/adapter'
 import { logger } from 'hono/logger'
 import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import OpenAI from 'openai'
+import { streamText, convertToCoreMessages } from 'ai'
+import { createOpenAI } from '@ai-sdk/openai'
 import { renderer } from './renderer'
 
 const app = new Hono()
@@ -23,18 +23,16 @@ const schema = z.object({
 app.post('/api/chat', zValidator('json', schema), async (c) => {
   const { messages } = c.req.valid('json')
 
-  const openai = new OpenAI({
+  const openai = createOpenAI({
     apiKey: env<{ OPENAI_API_KEY: string }>(c).OPENAI_API_KEY
   })
 
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages
+  const result = await streamText({
+	model: openai("gpt-3.5-turbo"),
+	messages: convertToCoreMessages(messages)
   })
 
-  const stream = OpenAIStream(response)
-  return new StreamingTextResponse(stream)
+  return result.toDataStreamResponse();
 })
 
 app.get('*', renderer, async (c) => {
